979777c3aa10dfe06653889f409045279d0ec6cb
bennyy117 <87014143+bennyy117@users.noreply.github.com>
2021-07-06 17:52:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Dino-kun!',
+    introDesc: `Nửa năm trôi qua từ lúc tớ thích cậu rùi ó :3`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    title: 'Tớ yêu cậu hơn bất cứ thứ gì khác ❤️',
+    desc: 'Cậu có yêu tớ không 🥺',
+    btnYes: 'Có chứ yêu cậu nhất trên đời lun ó 😷',
+    btnNo: 'Hông tgm',
+    question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại yêu tớ <3',
+    btnReply: 'Gửi cho tớ ❤️',
+    reply: 'Tại vì cậu cute á :3 Tớ yêu cậu nhiều lắm :33',
+    mess: 'Hí hí tớ biết mà 🥰. Tớ yêu cậu nhìu hơnn 😳',
+    messDesc: 'Nhớ cậu quá :<',
+    btnAccept: 'Ble 👿',
+    messLink: 'https://www.facebook.com/watashiwaLoandesu' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
